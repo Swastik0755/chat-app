d80c7ac8e0324bdae9e7415ee1c9f284f8705fb9
Swastik <sswastik2002@gmail.com>
2023-11-11 07:41:46 +0530

    10

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,13 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-const path = require("path");
+const path = require('node:path');
 
 dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json()); // to accept json data
+app.use(express.json());
 
 
 app.use("/api/user", userRoutes);
@@ -19,22 +19,22 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 const __dirname1 = path.resolve();
-const __dirname2 = path.resolve(__dirname1, "frontend", "build", "index.html")
 
 
-if (process.env.NODE_ENV === "productions") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>{
-    res.sendFile(path.resolve(__dirname1, "index.html"));
-  });
+    app.get("*", (req, res) =>{
+    
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  })
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
 }
 
-// Error Handling middlewares
+
 app.use(notFound);
 app.use(errorHandler);
 
