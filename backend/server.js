aacd1c0ad6bcf638cba44198f7150eb886572b94
Swastik <sswastik2002@gmail.com>
2023-11-11 08:29:45 +0530

    12

--- backend/server.js
+++ backend/server.js
@@ -21,18 +21,18 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 
 
-// if (process.env.NODE_ENV === "productions") {
-//     app.use(express.static(path.join(__dirname1, "/frontend/build")));
+if (process.env.NODE_ENV === "productions") {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-//     app.get("*", (req, res) =>{
+    app.get("*", (req, res) =>{
     
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-//   })
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 
 app.use(notFound);
